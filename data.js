7a8f70fe7e8d85c211f0655e4e9fa80b2371dbb3
fileExplorer10 <46820976+fileExplorer10@users.noreply.github.com>
2023-02-20 13:22:34 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+let songs = [
+    {
+        name:'505',
+        path: './assets/audio/505 (320 kbps).mp3',
+        artist: 'arctic monkeys',
+        cover: './assets/worst.jpg'
+
+    },
+    {
+        name:'Do I Wanna Know?',
+        path: './assets/audio/Arctic Monkeys - Do I Wanna Know_ (Official Video) (320 kbps).mp3',
+        artist: 'arctic monkeys',
+        cover: './assets/am.jpeg'
+    },
+    {
+        name:'R U mine',
+        path: './assets/audio/Arctic Monkeys - R U Mine_ (Official Video) (320 kbps).mp3',
+        artist: 'arctic monkeys',
+        cover: './assets/am.jpeg'
+    },
+    {
+        name:'Snap Out Of It',
+        path: './assets/audio/Snap Out Of It (320 kbps).mp3',
+        artist: 'arctic monkeys',
+        cover: './assets/am.jpeg'
+    },
+    {
+        name:'One Point Pespective',
+        path: './assets/audio/Arctic Monkeys - One Point Perspective (Official Audio) (320 kbps).mp3',
+        artist: 'arctic monkeys',
+        cover: './assets/tranquility.jpg'
+    },
+
+]
